fa0e0d181a0cccb3bbcda63886ae46586ba5cf1f
ligia-davila-bozzi <ligia@respondeai.com.br>
2021-08-24 09:50:31 -0300

    preparando review

--- src/components/IonIcon.js
+++
@@ -1,9 +0,0 @@
-const Icon = (props) => {
-    return (
-        <ion-button title={props.title} onClick={props.action}>
-            <ion-icon name={props.name} />
-        </ion-button>
-    )
-}
-
-export default Icon;
